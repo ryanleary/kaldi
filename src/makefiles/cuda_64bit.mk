c28c360b05530737817614bac12fdf33c10c45d6
Hugo Braun <hbraun@nvidia.com>
2018-08-17 13:40:24 -0700

    moved compile options to CUDA_FLAGS

--- src/makefiles/cuda_64bit.mk
+++ src/makefiles/cuda_64bit.mk
@@ -6,7 +6,7 @@ $(error CUDATKDIR not defined.)
 endif
 
 CUDA_INCLUDE= -I$(CUDATKDIR)/include
-CUDA_FLAGS = -g -Xcompiler -fPIC --verbose --machine 64 -DHAVE_CUDA \
+CUDA_FLAGS = -g -Xcompiler "-fPIC -pthread -isystem $(OPENFSTINC)" --verbose --machine 64 -DHAVE_CUDA \
              -ccbin $(CXX) -DKALDI_DOUBLEPRECISION=$(DOUBLE_PRECISION) \
              -std=c++11 -DCUDA_API_PER_THREAD_DEFAULT_STREAM
 
